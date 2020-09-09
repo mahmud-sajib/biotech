b950ecc88c3be75c5a231c6eac311a4ff917f434
mahmud-sajib <shout.mahmud@gmail.com>
2020-09-09 13:31:40 +0600

    first commit

---
+++ app/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from .models import MetaDataTable, OtuTable, OtuMetaTable, TaxonomicTable
+
+# Register your models here.
+
+class MetaDataTableAdmin(admin.ModelAdmin):
+    # Display columns in horizontal list
+    list_display = ('id', 'sample', 'index_1', 'index_2', 'group', 'diet', 'cage', 'sample_type', 'orgin_diet')
+    
+    # Columns having links
+    list_display_links = ('id', 'sample')
+
+class OtuTableAdmin(admin.ModelAdmin):
+    # Display columns in horizontal list
+    list_display = ('id', 'seq', 'sa_1', 'sa_2', 'sa_3', 'sa_4', 'sa_5', 'sa_6', 'sa_7', 'sa_8', 'sa_9', 'sa_10', 'sa_11', 'sa_12', 'sa_13', 'sa_14', 'sa_15')
+    
+    # Columns having links
+    list_display_links = ('id', 'seq')
+
+class OtuMetaTableAdmin(admin.ModelAdmin):
+    # Display columns in horizontal list
+    list_display = ('id', 'seq_id', 'sample_id', 'value')
+    
+    # Columns having links
+    list_display_links = ('id', 'seq_id')
+
+class TaxonomicTableAdmin(admin.ModelAdmin):
+    # Display columns in horizontal list
+    list_display = ('id', 'seq', 'kingdom', 'phylum', 'taconomy_class', 'order', 'family', 'genus', 'species')
+    
+    # Columns having links
+    list_display_links = ('id', 'seq')
+    
+admin.site.register(MetaDataTable, MetaDataTableAdmin)
+admin.site.register(OtuTable, OtuTableAdmin)
+admin.site.register(OtuMetaTable, OtuMetaTableAdmin)
+admin.site.register(TaxonomicTable, TaxonomicTableAdmin)
---
+++ app/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+# Create your models here.
+
+class MetaDataTable(models.Model):
+    sample = models.CharField(max_length=50)
+    index_1 = models.CharField(max_length=50)
+    index_2 = models.CharField(max_length=50)
+    group = models.CharField(max_length=50)
+    diet = models.CharField(max_length=50)
+    cage = models.CharField(max_length=50)
+    sample_type = models.CharField(max_length=50)
+    orgin_diet = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.sample
+
+
+class OtuTable(models.Model):
+    seq = models.CharField(max_length=100, null=True)
+    sa_1 = models.CharField(max_length=50, null=True)
+    sa_2 = models.CharField(max_length=50, null=True)
+    sa_3 = models.CharField(max_length=50, null=True)
+    sa_4 = models.CharField(max_length=50, null=True)
+    sa_5 = models.CharField(max_length=50, null=True)
+    sa_6 = models.CharField(max_length=50, null=True)
+    sa_7 = models.CharField(max_length=50, null=True)
+    sa_8 = models.CharField(max_length=50, null=True)
+    sa_9 = models.CharField(max_length=50, null=True)
+    sa_10 = models.CharField(max_length=50, null=True)
+    sa_11 = models.CharField(max_length=50, null=True)
+    sa_12 = models.CharField(max_length=50, null=True)
+    sa_13 = models.CharField(max_length=50, null=True)
+    sa_14 = models.CharField(max_length=50, null=True)
+    sa_15 = models.CharField(max_length=50, null=True)
+
+
+    def __str__(self):
+        return self.seq
+
+class OtuMetaTable(models.Model):
+    seq_id = models.ForeignKey(OtuTable, on_delete=models.SET_NULL, null=True)
+    sample_id = models.ForeignKey(MetaDataTable, on_delete=models.SET_NULL, null=True)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.seq_id} | {self.sample_id}" 
+
+
+class TaxonomicTable(models.Model):
+    seq = models.ForeignKey(OtuTable, on_delete=models.SET_NULL, null=True, related_name='taxonomics')
+    kingdom = models.CharField(max_length=50)
+    phylum = models.CharField(max_length=50)
+    taconomy_class = models.CharField(max_length=50)
+    order = models.CharField(max_length=50)
+    family = models.CharField(max_length=50)
+    genus = models.CharField(max_length=50)
+    species = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.kingdom
+
+
+
+
+
+
